94c79709a75d18f1333ddc701d027e99ac85bd3e
Darsssiiieee <darsie040@gmail.com>
2024-09-18 21:54:43 +0800

    #24todomanager

--- src/app/components/todomanager/todomanager.component.ts
+++ src/app/components/todomanager/todomanager.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrl: './todomanager.component.css'
 })
 export class TodomanagerComponent {
+  tasks: string[] = [];
+  newTask: string = '';
+
+  addTask() {
+    if (this.newTask.trim()) {
+      this.tasks.push(this.newTask.trim());
+      this.newTask = '';
+    }
+  }
+
+  removeTask(index: number) {
+    this.tasks.splice(index, 1);
+  }
 
 }
