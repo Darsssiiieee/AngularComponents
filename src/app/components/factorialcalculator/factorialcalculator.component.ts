786d427a5e7339a23fd628761d41a3d01f1cda41
Darsssiiieee <darsie040@gmail.com>
2024-09-18 21:51:19 +0800

    #23factorialcalculator

--- src/app/components/factorialcalculator/factorialcalculator.component.ts
+++ src/app/components/factorialcalculator/factorialcalculator.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrl: './factorialcalculator.component.css'
 })
 export class FactorialcalculatorComponent {
+  number: number | null = null;
+  factorialResult: number | null = null;
 
+  calculateFactorial() {
+    if (this.number !== null && this.number >= 0) {
+      this.factorialResult = this.factorial(this.number);
+    }
+  }
+
+  factorial(n: number): number {
+    if (n === 0 || n === 1) {
+      return 1;
+    }
+    return n * this.factorial(n - 1);
+  }
 }
