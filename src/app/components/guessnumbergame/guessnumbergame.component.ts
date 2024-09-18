a16f0318a09d57acf917d3419d70a22ca198aeab
Darsssiiieee <darsie040@gmail.com>
2024-09-18 22:00:56 +0800

    #25guessnumbergame

--- src/app/components/guessnumbergame/guessnumbergame.component.ts
+++ src/app/components/guessnumbergame/guessnumbergame.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrl: './guessnumbergame.component.css'
 })
 export class GuessnumbergameComponent {
+  maxNumber: number = 100;
+  targetNumber!: number; 
+  userGuess: number | null = null;
+  message: string | null = null;
 
+  constructor() {
+    this.resetGame();
+  }
+
+  checkGuess() {
+    if (this.userGuess === this.targetNumber) {
+      this.message = 'Congratulations! You guessed the number!';
+    } else if (this.userGuess! < this.targetNumber) {
+      this.message = 'Too low! Try again.';
+    } else if (this.userGuess! > this.targetNumber) {
+      this.message = 'Too high! Try again.';
+    }
+  }
+
+  resetGame() {
+    this.targetNumber = Math.floor(Math.random() * this.maxNumber) + 1;
+    this.userGuess = null;
+    this.message = null;
+  }
 }
