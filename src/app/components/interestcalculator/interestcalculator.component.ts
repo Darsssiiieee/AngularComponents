c4910eadcebdc1d7dc08ea4a2efa08bf6e325f20
Darsssiiieee <darsie040@gmail.com>
2024-09-18 22:55:28 +0800

    #33interestcalculator

--- src/app/components/interestcalculator/interestcalculator.component.ts
+++ src/app/components/interestcalculator/interestcalculator.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrl: './interestcalculator.component.css'
 })
 export class InterestcalculatorComponent {
+  principal: number | null = null;
+  rate: number | null = null;
+  time: number | null = null;
+  interest: number | null = null;
 
+  calculateInterest() {
+    if (this.principal !== null && this.rate !== null && this.time !== null) {
+      this.interest = (this.principal * this.rate * this.time) / 100;
+    }
+  }
 }
