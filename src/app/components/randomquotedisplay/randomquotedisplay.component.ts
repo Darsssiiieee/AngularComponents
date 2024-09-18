e48ff5880351a67cb1976aa3901c465a436a3216
Darsssiiieee <darsie040@gmail.com>
2024-09-18 23:52:46 +0800

    #38randomquotedisplay

--- src/app/components/randomquotedisplay/randomquotedisplay.component.ts
+++ src/app/components/randomquotedisplay/randomquotedisplay.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrl: './randomquotedisplay.component.css'
 })
 export class RandomquotedisplayComponent {
+  quotes: string[] = [
+    "The only limit to our realization of tomorrow will be our doubts of today. — Franklin D. Roosevelt",
+    
+    "In the end, we will remember not the words of our enemies, but the silence of our friends. — Martin Luther King Jr.",
+    
+    "Life is either a daring adventure or nothing at all. — Helen Keller",
+    
+    "The future belongs to those who believe in the beauty of their dreams. — Eleanor Roosevelt",
+    
+    "Do not wait to strike till the iron is hot, but make it hot by striking. — William Butler Yeats"
+  ];
 
+  selectedQuote: string | null = null;
+
+  getRandomQuote() {
+    const randomIndex = Math.floor(Math.random() * this.quotes.length);
+    this.selectedQuote = this.quotes[randomIndex];
+  }
 }
