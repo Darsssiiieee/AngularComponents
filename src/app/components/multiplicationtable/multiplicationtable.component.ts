4ebf6fa24f6afce8da73959124be2749461a8946
Darsssiiieee <darsie040@gmail.com>
2024-09-18 20:48:55 +0800

    #15multiplicationtable

--- src/app/components/multiplicationtable/multiplicationtable.component.ts
+++ src/app/components/multiplicationtable/multiplicationtable.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrl: './multiplicationtable.component.css'
 })
 export class MultiplicationtableComponent {
+  number: number | null = null;
+  table: number[] = [];
 
+  generateTable() {
+    if (this.number !== null) {
+      this.table = Array.from({ length: 10 }, (_, i) => (i + 1) * this.number!);
+    } else {
+      this.table = [];
+    }
+  }
 }
