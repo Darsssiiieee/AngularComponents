7bbca3daa31864fdec58ea4fa3b5e8544099a308
Darsssiiieee <darsie040@gmail.com>
2024-09-18 23:22:02 +0800

    #34compoundinterestcalculator

--- src/app/components/compoundinterestcalculator/compoundinterestcalculator.component.ts
+++ src/app/components/compoundinterestcalculator/compoundinterestcalculator.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrl: './compoundinterestcalculator.component.css'
 })
 export class CompoundinterestcalculatorComponent {
+  principal: number | null = null;
+  rate: number | null = null;
+  time: number | null = null;
+  frequency: number | null = null;
+  compoundInterest: number | null = null;
+
+  calculateCompoundInterest() {
+    if (this.principal !== null && this.rate !== null && this.time !== null && this.frequency !== null) {
+      const P = this.principal;
+      const r = this.rate / 100;
+      const t = this.time;
+      const n = this.frequency;
+      this.compoundInterest = P * Math.pow((1 + r / n), n * t) - P;
+    }
+  }
 
 }
