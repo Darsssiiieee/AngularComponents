5263af978e08aba171a2dd874b7e9055fea57998
Darsssiiieee <darsie040@gmail.com>
2024-09-18 23:27:11 +0800

    #35fibonaccigenerator

--- src/app/components/fibonaccigenerator/fibonaccigenerator.component.ts
+++ src/app/components/fibonaccigenerator/fibonaccigenerator.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrl: './fibonaccigenerator.component.css'
 })
 export class FibonaccigeneratorComponent {
+  n: number | null = null;
+  fibonacciSequence: number[] = [];
 
+  generateFibonacci() {
+    if (this.n !== null && this.n > 0) {
+      const sequence = [0, 1];
+      while (sequence.length < this.n) {
+        const nextNumber = sequence[sequence.length - 1] + sequence[sequence.length - 2];
+        sequence.push(nextNumber);
+      }
+      this.fibonacciSequence = sequence.slice(0, this.n);
+    }
+  }
 }
