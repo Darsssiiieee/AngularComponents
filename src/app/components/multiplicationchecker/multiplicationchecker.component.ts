42472e2920c8854a8509139d7026457ffe6457f7
Darsssiiieee <darsie040@gmail.com>
2024-09-18 22:21:54 +0800

    #28multiplicationchecker

--- src/app/components/multiplicationchecker/multiplicationchecker.component.ts
+++ src/app/components/multiplicationchecker/multiplicationchecker.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrl: './multiplicationchecker.component.css'
 })
 export class MultiplicationcheckerComponent {
+  number: number | null = null;
+  divisor: number | null = null;
+  isMultiple: boolean | null = null;
+  resultMessage: string = '';
 
+  checkMultiple() {
+    if (this.number !== null && this.divisor !== null) {
+      if (this.divisor === 0) {
+        this.resultMessage = 'Divisor cannot be zero.';
+        this.isMultiple = null;
+      } else {
+        this.isMultiple = this.number % this.divisor === 0;
+        this.resultMessage = this.isMultiple
+          ? `${this.number} is a multiple of ${this.divisor}.`
+          : `${this.number} is not a multiple of ${this.divisor}.`;
+      }
+    }
+  }
 }
