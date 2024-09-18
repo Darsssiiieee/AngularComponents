62c0e412b1f1824abcf6c07d7d1937e73c80b3fa
Darsssiiieee <darsie040@gmail.com>
2024-09-18 21:38:30 +0800

    #21temperatureconverter

--- src/app/components/temperatureconverter/temperatureconverter.component.ts
+++ src/app/components/temperatureconverter/temperatureconverter.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrl: './temperatureconverter.component.css'
 })
 export class TemperatureconverterComponent {
+  temperature: number | null = null;
+  conversionType: string = 'celsiusToFahrenheit';
+  convertedTemperature: number | null = null;
 
+  convertTemperature() {
+    if (this.temperature !== null) {
+      if (this.conversionType === 'celsiusToFahrenheit') {
+        this.convertedTemperature = (this.temperature * 9/5) + 32;
+      } else if (this.conversionType === 'fahrenheitToCelsius') {
+        this.convertedTemperature = (this.temperature - 32) * 5/9;
+      }
+    }
+  }
 }
