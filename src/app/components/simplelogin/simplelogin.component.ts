d4e00bae2eeefde05470530db0706971d0c5ade3
Darsssiiieee <darsie040@gmail.com>
2024-09-18 20:54:51 +0800

    #16simplelogin

--- src/app/components/simplelogin/simplelogin.component.ts
+++ src/app/components/simplelogin/simplelogin.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrl: './simplelogin.component.css'
 })
 export class SimpleloginComponent {
+  email: string = '';
+  password: string = '';
+  submitted: boolean = false;
 
+  onSubmit() {
+    this.submitted = true;
+    console.log('Email:', this.email);
+    console.log('Password:', this.password);
+  }
 }
