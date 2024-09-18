b6e2116e34204e33bd5a9ea99b5daeba4c255632
Darsssiiieee <darsie040@gmail.com>
2024-09-18 21:06:23 +0800

    #18bookmarklist

--- src/app/components/bookmarklist/bookmarklist.component.ts
+++ src/app/components/bookmarklist/bookmarklist.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrl: './bookmarklist.component.css'
 })
 export class BookmarklistComponent {
+  urlInput: string = '';
+  urlList: string[] = [];
 
+  addUrl() {
+    if (this.urlInput.trim()) {
+      if (!this.urlInput.startsWith('http://') && !this.urlInput.startsWith('https://')) {
+        this.urlInput = 'https://' + this.urlInput;
+      }
+      this.urlList.push(this.urlInput);
+      this.urlInput = '';
+    }
+  }
 }
