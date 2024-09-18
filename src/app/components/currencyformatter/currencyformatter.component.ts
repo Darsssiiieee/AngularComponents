ff2becb9c244b0f1d479c9d449d847984da3b459
Darsssiiieee <darsie040@gmail.com>
2024-09-18 23:42:16 +0800

    #37currencyformatter

--- src/app/components/currencyformatter/currencyformatter.component.ts
+++ src/app/components/currencyformatter/currencyformatter.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrl: './currencyformatter.component.css'
 })
 export class CurrencyformatterComponent {
+  amount: number | null = null;
+  formattedDollar: string | null = null;
+  formattedPhp: string | null = null;
+  formattedEuro: string | null = null;
 
+  euroConversionRate = 0.92;
+  phpConversionRate = 56;
+
+  formatCurrency() {
+    if (this.amount !== null) {
+      this.formattedDollar = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD'
+      }).format(this.amount);
+
+      this.formattedPhp = new Intl.NumberFormat('en-PH', {
+        style: 'currency',
+        currency: 'PHP'
+      }).format(this.amount * this.phpConversionRate);
+
+      this.formattedEuro = new Intl.NumberFormat('en-IE', {
+        style: 'currency',
+        currency: 'EUR'
+      }).format(this.amount * this.euroConversionRate);
+    }
+  }
 }
