8f06710862cdb71df8ad3f5b353ce62e06961174
Darsssiiieee <darsie040@gmail.com>
2024-09-18 22:12:30 +0800

    #27randomnumbergenerator

--- src/app/components/randomnumbergenerator/randomnumbergenerator.component.ts
+++ src/app/components/randomnumbergenerator/randomnumbergenerator.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrl: './randomnumbergenerator.component.css'
 })
 export class RandomnumbergeneratorComponent {
+  minRange: number = 0;
+  maxRange: number = 100;
+  randomNumber: number | null = null;
+
+  generateRandomNumber() {
+    if (this.minRange < this.maxRange) {
+      this.randomNumber = Math.floor(Math.random() * (this.maxRange - this.minRange + 1)) + this.minRange;
+    } else {
+      this.randomNumber = null;
+    }
+  }
 
 }
