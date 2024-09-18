2eb57b0852d75fef026480c48066186ee8ee99fe
Darsssiiieee <darsie040@gmail.com>
2024-09-18 20:05:28 +0800

    #8calculator

--- src/app/components/calculator/calculator.component.ts
+++ src/app/components/calculator/calculator.component.ts
@@ -6,5 +6,36 @@ import { Component } from '@angular/core';
   styleUrl: './calculator.component.css'
 })
 export class CalculatorComponent {
+  num1: number | null = null;
+  num2: number | null = null;
+  result: number | null = null;
 
+  add() {
+    if (this.num1 !== null && this.num2 !== null) {
+      this.result = this.num1 + this.num2;
+    }
+  }
+
+  subtract() {
+    if (this.num1 !== null && this.num2 !== null) {
+      this.result = this.num1 - this.num2;
+    }
+  }
+
+  multiply() {
+    if (this.num1 !== null && this.num2 !== null) {
+      this.result = this.num1 * this.num2;
+    }
+  }
+
+  divide() {
+    if (this.num1 !== null && this.num2 !== null) {
+      if (this.num2 !== 0) {
+        this.result = this.num1 / this.num2;
+      } else {
+        this.result = null;
+        alert('Cannot divide by zero');
+      }
+    }
+  }
 }
