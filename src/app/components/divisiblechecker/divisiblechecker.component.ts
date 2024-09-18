014620eede08432126091d77e364082ba9e75d43
Darsssiiieee <darsie040@gmail.com>
2024-09-19 00:02:41 +0800

    #40divisiblechecker

--- src/app/components/divisiblechecker/divisiblechecker.component.ts
+++ src/app/components/divisiblechecker/divisiblechecker.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrl: './divisiblechecker.component.css'
 })
 export class DivisiblecheckerComponent {
+  number1: number | null = null;
+  number2: number | null = null;
+  result: string | null = null; 
 
+  checkDivisibility() {
+    if (this.number1 !== null && this.number2 !== null) {
+      if (this.number2 === 0) {
+        this.result = 'Division by zero is not allowed.';
+      } else if (this.number1 % this.number2 === 0) {
+        this.result = `${this.number1} is divisible by ${this.number2}.`;
+      } else {
+        this.result = `${this.number1} is not divisible by ${this.number2}.`;
+      }
+    }
+  }
 }
