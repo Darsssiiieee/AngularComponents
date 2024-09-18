9e643e8a8d89f5dd4a1235d602bb487bf6c6c54f
Darsssiiieee <darsie040@gmail.com>
2024-09-18 22:43:23 +0800

    #32usernamevalidator

--- src/app/components/usernamevalidator/usernamevalidator.component.ts
+++ src/app/components/usernamevalidator/usernamevalidator.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrl: './usernamevalidator.component.css'
 })
 export class UsernamevalidatorComponent {
+  username: string = '';
+  validationMessage: string | null = null;
+  private minLength: number = 6;
+  private maxLength: number = 20;
+  private regex: RegExp = /^[a-zA-Z0-9_]*$/;
 
+  validateUsername() {
+    if (!this.username) {
+      this.validationMessage = 'Username is required.';
+    } else if (this.username.length < this.minLength) {
+      this.validationMessage = `Username must be at least ${this.minLength} characters long.`;
+    } else if (this.username.length > this.maxLength) {
+      this.validationMessage = `Username cannot exceed ${this.maxLength} characters.`;
+    } else if (!this.regex.test(this.username)) {
+      this.validationMessage = 'Username can only contain letters, numbers, and underscores.';
+    } else {
+      this.validationMessage = 'Username is valid!';
+    }
+  }
 }
